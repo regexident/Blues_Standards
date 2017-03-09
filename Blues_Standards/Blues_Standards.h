3bd70ef51595b12dd892e0aa19959783c0b2f0b0
Vincent Esche <regexident@gmail.com>
2017-03-09 16:44:16 +0100

    Initial Commit

---
+++ Blues_Standards/Blues_Standards.h
@@ -0,0 +1,19 @@
+//
+//  Blues_Standards.h
+//  Blues_Standards
+//
+//  Created by Vincent Esche on 09/03/2017.
+//  Copyright © 2017 NWTN Berlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Blues_Standards.
+FOUNDATION_EXPORT double Blues_StandardsVersionNumber;
+
+//! Project version string for Blues_Standards.
+FOUNDATION_EXPORT const unsigned char Blues_StandardsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Blues_Standards/PublicHeader.h>
+
+
